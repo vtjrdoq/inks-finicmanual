c54e85b8df4ac9c7badb27b23976305637b17d67
vtjrdoq <vtjrdoq@mail.ru>
2026-05-22 20:58:41 +0300

    Add files via upload

---
+++ animationPictures.js
@@ -0,0 +1,16 @@
+window.addEventListener('scroll', () => {
+    const container = document.querySelector('.hero__picture');
+    const picFinic = document.querySelector('.image--finic');
+    const picStudents = document.querySelector('.image--students');
+
+    const viewportCenter = window.innerHeight / 2;
+
+    const rect = container.getBoundingClientRect();
+    const containerCenter = rect.top + rect.height / 2;
+
+    const distanceFromCenter = containerCenter - viewportCenter;
+    const movement = Math.abs(distanceFromCenter) * 0.4;
+
+    picFinic.style.transform = `translateX(-${movement}px)`;
+    picStudents.style.transform = `translateX(${movement}px)`;
+})

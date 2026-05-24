ac3d17e498ead934b656413c2422bcba82e08ecb
vtjrdoq <vtjrdoq@mail.ru>
2026-05-24 14:50:27 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -3,3 +3,19 @@ fetch('header.html')
     .then(data => {
         document.getElementById('header-placeholder').innerHTML = data;
     });
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const stickyElement = document.querySelector('.nav__container');
+    if (!stickyElement) return;
+
+    const stickyInitialTop = stickyElement.getBoundingClientRect().top + window.scrollY;
+
+    window.addEventListener('scroll', () => {
+        if (window.scrollY >= stickyInitialTop) {
+            stickyElement.classList.add('is-sticky');
+        } else {
+            stickyElement.classList.remove('is-sticky')
+        }
+    })
+})

3843fda09d8f9476156017a6420fad0e89c7cc10
vtjrdoq <vtjrdoq@mail.ru>
2026-05-27 08:17:57 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -6,6 +6,12 @@ fetch('header.html')
         document.getElementById('header-placeholder').innerHTML = data;
     });
 
+fetch('footer.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('footer-placeholder').innerHTML = data;
+    });
+
 
 // замена стики
 
